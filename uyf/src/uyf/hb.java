1387d5c5cedaec271baa3c2c91e7c1fa2386e4af
sdac-9 <sdac-9@sdac-9-PC>
2024-09-25 14:18:07 +0530

    my 1st

---
+++ uyf/src/uyf/hb.java
@@ -0,0 +1,11 @@
+package uyf;
+
+public class hb{
+public static void main(String[] args) {
+		System.out.println("main project");
+		System.out.println("file....");
+		System.out.println("1st");
+		System.out.println("github");
+	}
+	}
+
